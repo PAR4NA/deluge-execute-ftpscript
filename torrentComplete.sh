a33caa69232145e93cbf8b1ea104113b003cee96
Peter Den Hartog <pddenhar@gmail.com>
2013-12-06 17:38:59 -0600

    Fixed single file uploads

--- torrentComplete.sh
+++ torrentComplete.sh
@@ -6,21 +6,34 @@ torrentpath=$3
 HOST='HOSTNAME.com'
 USER='USER'
 PASS='PASSWORD'
-STOREAGEDIR='/media/sde1/home/user/private/Storage'
 
 echo "Torrent Details: ***" $torrentname "***" $torrentpath "***" $torrentid "***"  >> ~/execute_script.log
 
-if [ "$STOREAGEDIR/What.cd" == "$torrentpath" ]
+if [ "/media/sde1/home/spammandan/private/Storage/What.cd" == "$torrentpath" ]
 then
-echo "This is a what.cd torrent, sending with FTP" >> ~/execute_script.log
-lftp -u $USER,$PASS $HOST -e "mirror -P4 -R \"$torrentpath/$torrentname\" /Music/What.cd/; quit"
-elif [ "$STOREAGEDIR/AwesomeHD" == "$torrentpath" ]
+	echo "This is a what.cd torrent, sending with FTP" >> ~/execute_script.log
+		lftp -u $USER,$PASS $HOST -e "mirror -P4 -R \"$torrentpath/$torrentname\" /Music/What.cd/; quit"  &>> ~/execute_script.log
+elif [ "/media/sde1/home/spammandan/private/Storage/AwesomeHD" == "$torrentpath" ]
 then
-echo "This is an AwesomeHD torrent, sending with FTP" >> ~/execute_script.log
-lftp -u $USER,$PASS $HOST -e "mirror -P4 -R \"$torrentpath/$torrentname\" /Subsonic/Movies/Uncategorized/; quit"
-elif [ "$STOREAGEDIR/TV Shows" == "$torrentpath" ]
+	echo "This is an AwesomeHD torrent, sending with FTP" >> ~/execute_script.log
+	if [ -f "$torrentpath/$torrentname" ]
+	then
+		echo "     Torrent is a single file" >> ~/execute_script.log
+                lftp -u $USER,$PASS $HOST -e "cd /Subsonic/Movies/Uncategorized/; put \"$torrentpath/$torrentname\"; quit" &>> ~/execute_script.log
+	else
+		echo "     Torrent is a folder" >> ~/execute_script.log
+		lftp -u $USER,$PASS $HOST -e "mirror -P4 -R \"$torrentpath/$torrentname\" /Subsonic/Movies/Uncategorized/; quit" &>> ~/execute_script.log
+	fi
+elif [ "/media/sde1/home/spammandan/private/Storage/TV Shows" == "$torrentpath" ]
 then
-echo "This is a TV Show torrent, skipping" >> ~/execute_script.log
-
+        echo "This is an TV Show torrent, sending with FTP" >> ~/execute_script.log
+        if [ -f "$torrentpath/$torrentname" ]
+        then
+                echo "     Torrent is a single file" >> ~/execute_script.log
+                lftp -u $USER,$PASS $HOST -e "cd Subsonic/TV\ Shows/Uncategorized/; put \"$torrentpath/$torrentname\"; quit" &>> ~/execute_script.log
+        else
+                echo "     Torrent is a folder" >> ~/execute_script.log
+                lftp -u $USER,$PASS $HOST -e "mirror -P4 -R \"$torrentpath/$torrentname\" \"/Subsonic/TV\ Shows/Uncategorized/\"; quit" &>> ~/execute_script.log
+        fi
 
 fi
